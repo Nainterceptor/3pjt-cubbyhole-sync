362cb99a2d91c83a84f51262060fbd77972b899f
Fireaxe <axess9fireaxe@gmail.com>
2014-02-24 17:19:44 +0100

    Début du restful client

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtNetwork>
+
+static const char *REQUEST_URL = "http://localhost:3000/user/login";
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    network = new QNetworkAccessManager(this);
+    QNetworkRequest request;
+
+    request.setUrl(QUrl(REQUEST_URL));
+    QNetworkReply *reply = network->get(request);
+
     ui->setupUi(this);
 }
 
